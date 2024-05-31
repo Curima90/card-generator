bbc1d19aeb83faa05d829608ecc4c1dfec83b7e6
Maurizio Curione <mauriziocurione@Maurizios-MBP.station>
2024-05-31 19:39:12 +0100

    working on assignment

--- src/app.js
+++ src/app.js
@@ -1,3 +1,23 @@
-window.onload = function() {
-  
+window.onload = function () {
 };
+
+function cardNumber() {
+  let number = Math.floor((Math.random() * 12) + 1)
+  if (number = "11") {
+    number = "J";
+  }
+  else if (number = "12") {
+
+    number = "Q";
+  }
+  else if (number = "13") {
+
+    number = "K";
+  }
+  else if (number = "1") {
+
+    number = "A"
+  }
+  return number;
+}
+
